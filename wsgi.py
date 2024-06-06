9e34df5803c3bb7a2618e418ff14b0aca135b0a3
vaibhav-purohit <vaibhavpurohit@fynd-external.com>
2024-06-06 15:29:38 +0530

    added wsgi and git ignore file

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
-from app import create_app  # Assuming you have a create_app function in app.py
+from app import app  # Assuming you have a create_app function in app.py
 
-app = create_app()  # Create an instance of your Flask app
 
 if __name__ == "__main__":
     app.run()
